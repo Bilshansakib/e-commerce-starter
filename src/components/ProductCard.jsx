4720ae2a4cdab7217b2ba2eebf22a046e676dab7
Bilshansakib <burakaghani@gmail.com>
2024-08-17 01:22:01 +0600

    Fixed: PropType of product page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,9 @@
 import { Card } from "antd";
-
+import PropTypes from "prop-types";
+import { BiSolidCategory } from "react-icons/bi";
+import { IoIosPricetag } from "react-icons/io";
+import { PiShootingStarFill } from "react-icons/pi";
+import { TbBrandAppgallery } from "react-icons/tb";
 const { Meta } = Card;
 
 const ProductCard = ({ products }) => {
@@ -23,37 +27,55 @@ const ProductCard = ({ products }) => {
   };
   return (
     <>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2"> 
-    {products.map((data) => (
-        <>
-          {/* <ProductCard key={data._id} products={products}></ProductCard> */}
-          <div key={data._id} >
-            <Card
-              hoverable
-              style={{ width: 300 }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                />
-              }
-            >
-              <Meta
-                title={data.ratings}
-                description={data.description}
-              />
-              <Card.Grid style={gridStyle}>{data.productName}</Card.Grid>
-              <Card.Grid style={gridStyle}>{data.brandName}</Card.Grid>
-              <Card.Grid style={gridStyle}>{data.category}</Card.Grid>
-              <Card.Grid style={gridStyle}>{data.price}</Card.Grid>
-            </Card>
-          </div>
-        </>
-      ))}
-    </div>
-     
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+        {products.map((data) => (
+          <>
+            {/* <ProductCard key={data._id} products={products}></ProductCard> */}
+            <div key={data._id} className="">
+              <Card
+                hoverable
+                style={{ width: 300 }}
+                cover={
+                  <img
+                    className="h-52  bg-cover"
+                    alt="example"
+                    src={data.productImage}
+                  />
+                }
+              >
+                <div className="flex p-2 items-center gap-2">
+                  <PiShootingStarFill />
+                  <Meta  title={data.ratings} />
+                  
+                </div>
+                <Meta className="p-2" description={data.description} />
+                <Card.Grid style={gridStyle}>{data.productName}</Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <div className="flex items-center gap-2">
+                    <TbBrandAppgallery /> {data.brandName}
+                  </div>
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <div className="flex items-center gap-2">
+                    <BiSolidCategory /> {data.category}
+                  </div>
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <div className="flex items-center gap-2">
+                    <IoIosPricetag /> ${data.price}
+                  </div>
+                </Card.Grid>
+              </Card>
+            </div>
+          </>
+        ))}
+      </div>
     </>
   );
+};
+
+ProductCard.propTypes = {
+  products: PropTypes.any,
 };
 
 export default ProductCard;
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
-
-
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,7 +9,7 @@ const ProductPage = () => {
   const [priceRange, setPriceRange] = useState([0, 9999]);
   const [price, setPrice] = useState("");
   const [sort, setSort] = useState("");
-  
+
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -25,8 +23,10 @@ const ProductPage = () => {
           queryParams.append("maxPrice", priceRange[1]);
         if (price) queryParams.append("price", price);
         if (sort) queryParams.append("sort", sort);
-        const url = await fetch(`http://localhost:9000/api/products?${queryParams}`);
-        
+        const url = await fetch(
+          `http://localhost:9000/api/products?${queryParams}`
+        );
+
         const data = await url.json();
         console.log(data);
         setProducts(data);
@@ -49,7 +49,7 @@ const ProductPage = () => {
   const handlePrice = (e) => {
     setPrice(e.target.value);
   };
-  
+
   const handlePriceRange = (e, type) => {
     const newPrice = [...priceRange];
     if (type === "min") {
@@ -63,98 +63,96 @@ const ProductPage = () => {
     setSort(e.target.value);
   };
 
-
-console.log(products);
+  console.log(products);
 
   return (
     <div>
-        <div className="flex justify-center">
-      <input
-        type="text"
-        className="p-2 w-60 border border-gray-300 rounded mr-2"
-        placeholder="search products..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <input
-        type="text"
-        className="p-2 w-60 border border-gray-300 rounded mr-2"
-        placeholder="search price"
-        value={price}
-        onChange={handlePrice}
-      />
-      <select
-        className="p-2 w-60 border border-gray-300 rounded mr-2"
-        value={category}
-        onChange={handleCategory}
-      >
-         <option value="">Category</option>
-        <option value="Electronics">Electronics</option>
-        <option value="Computers">Computers</option>
-        <option value="Accessories">Accessories</option>
-        <option value="Audio">Audio</option>
-        <option value="Photography">Photography</option>
-        <option value="Wearables">Wearables</option>
-        <option value="Gaming">Gaming</option>
-        <option value="Tablets">Tablets</option>
-        <option value="Smart Home">Smart Home</option>
-        <option value="Health">Health</option>
-        <option value="Home Appliances">Home Appliances</option>
-       
-      </select>
-      <select
-        className="p-2 w-60 border border-gray-300 rounded mr-2"
-        value={brand}
-        onChange={handleOrder}
-      >
-         <option value="">Brand Name</option>
-        <option value="SoundWave">SoundWave</option>
-        <option value="VisionX">VisionX</option>
-        <option value="GamerPro">GamerPro</option>
-        <option value="FlexFit">FlexFit</option>
-        <option value="HydroFlow">HydroFlow</option>
-        <option value="FitPulse">FitPulse</option>
-        <option value="KitchenPro">KitchenPro</option>
-        <option value="ChargeIt">ChargeIt</option>
-        <option value="SnapShot">SnapShot</option>
-        <option value="TechGuru">TechGuru</option>
-        <option value="CleanHome">CleanHome</option>
-        <option value="UrbanStyle">UrbanStyle</option>
-        <option value="BrewMaster">BrewMaster</option>
-      </select>
-      <div className="flex border">
-        <input
-          type="number"
-          className="p-2 w-20 h-3/4 border border-gray-300 rounded ml-2"
-          placeholder="min price"
-          value={priceRange[0]}
-          onChange={(e) => handlePriceRange(e, "min")}
-        />
-        <div>
-          <p>To</p>
-        </div>
+      <div className="flex justify-center pt-2 pb-4">
         <input
           type="text"
-          className="p-2 w-20 h-3/4 border border-gray-300 rounded ml-2"
-          placeholder="min price"
-          value={priceRange[1]}
-          onChange={(e) => handlePriceRange(e, "max")}
+          className="p-2 w-60 border border-gray-300 rounded mr-2"
+          placeholder="search products..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <input
+          type="text"
+          className="p-2 w-60 border border-gray-300 rounded mr-2"
+          placeholder="search price"
+          value={price}
+          onChange={handlePrice}
         />
         <select
-          className="p-2 bordder border-gray-300 rounded mx-2"
-          value={sort}
-          onChange={handleSort}
+          className="p-2 w-60 border border-gray-300 rounded mr-2"
+          value={category}
+          onChange={handleCategory}
         >
-          <option value="">Price Order</option>
-          <option value="asc">Price: Low to High</option>
-          <option value="desc">Price: High to Low</option>
+          <option value="">Category</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Computers">Computers</option>
+          <option value="Accessories">Accessories</option>
+          <option value="Audio">Audio</option>
+          <option value="Photography">Photography</option>
+          <option value="Wearables">Wearables</option>
+          <option value="Gaming">Gaming</option>
+          <option value="Tablets">Tablets</option>
+          <option value="Smart Home">Smart Home</option>
+          <option value="Health">Health</option>
+          <option value="Home Appliances">Home Appliances</option>
         </select>
+        <select
+          className="p-2 w-60 border border-gray-300 rounded mr-2"
+          value={brand}
+          onChange={handleOrder}
+        >
+          <option value="">Brand Name</option>
+          <option value="SoundWave">SoundWave</option>
+          <option value="VisionX">VisionX</option>
+          <option value="GamerPro">GamerPro</option>
+          <option value="FlexFit">FlexFit</option>
+          <option value="HydroFlow">HydroFlow</option>
+          <option value="FitPulse">FitPulse</option>
+          <option value="KitchenPro">KitchenPro</option>
+          <option value="ChargeIt">ChargeIt</option>
+          <option value="SnapShot">SnapShot</option>
+          <option value="TechGuru">TechGuru</option>
+          <option value="CleanHome">CleanHome</option>
+          <option value="UrbanStyle">UrbanStyle</option>
+          <option value="BrewMaster">BrewMaster</option>
+        </select>
+        <div className="flex border">
+          <input
+            type="number"
+            className="p-2 w-20 h-3/4 border border-gray-300 rounded ml-2"
+            placeholder="min price"
+            value={priceRange[0]}
+            onChange={(e) => handlePriceRange(e, "min")}
+          />
+          <div>
+            <p>To</p>
+          </div>
+          <input
+            type="text"
+            className="p-2 w-20 h-3/4 border border-gray-300 rounded ml-2"
+            placeholder="min price"
+            value={priceRange[1]}
+            onChange={(e) => handlePriceRange(e, "max")}
+          />
+          <select
+            className="p-2 bordder border-gray-300 rounded mx-2"
+            value={sort}
+            onChange={handleSort}
+          >
+            <option value="">Price Order</option>
+            <option value="asc">Price: Low to High</option>
+            <option value="desc">Price: High to Low</option>
+          </select>
+        </div>
       </div>
-    </div >
-    <ProductCard  products={products}></ProductCard>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
 
-    </div>
+      <div className="border-2 flex justify-center items-center p-4">
+        <ProductCard products={products}></ProductCard>
+      </div>
     </div>
   );
 };
